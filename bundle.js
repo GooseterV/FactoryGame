7b64511be5b482ee82bbb32247fee85b0fef5194
GooseterV <goosevincent@gmail.com>
2021-10-09 15:45:10 -0400

    upped level amounts

--- bundle.js
+++ bundle.js
@@ -31,8 +31,8 @@ class Factory {
         if (this.gainAmount == 1) {
             this.gainAmount = 2
         }
-        this.gainAmount **= 1.3 //+= this.gainAmount / 100 * 75
-        this.levelUpgradePrice **= 1.275
+        this.gainAmount **= 1.35 //+= this.gainAmount / 100 * 75
+        this.levelUpgradePrice **= 1.25
         
     }
 }
